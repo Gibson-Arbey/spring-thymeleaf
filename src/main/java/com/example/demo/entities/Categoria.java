f0302e154bbc0e6679b836619c496701574ce185
Gibson-Arbey <gibsonarbey@gmail.com>
2023-05-09 17:14:58 -0500

    se adiciona todo lo relacionado a noticias

--- src/main/java/com/example/demo/entities/Categoria.java
+++ src/main/java/com/example/demo/entities/Categoria.java
@@ -1,10 +1,16 @@
 package com.example.demo.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +19,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "categorianews")
 public class Categoria {
 
 	@Id
-	@SequenceGenerator(name="categoria_id_seq", allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="categoria_id_seq")
+	@SequenceGenerator(name="categorianews_id_seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="categorianews_id_seq")
 	private Integer id;
 	
 	private String descripcion;
+	
+	 
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "categoria")
+	private List<News> news = new ArrayList<>();
 }
